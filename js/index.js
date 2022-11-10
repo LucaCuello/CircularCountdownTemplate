b490c9527f281ed83a58fd36eb28173d019234ec
Luca <lucagcuello@gmail.com>
2022-11-10 05:40:24 -0300

    Added new function parameter/option

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ const circularCountdown = (
   seconds = 60,
   animationTransition = "linear",
   color = "#c39fe0",
-  size = 500
+  size = 500,
+  startPosition = "right"
 ) => {
   // Rendering the seconds on screen
   let time = seconds;
@@ -23,6 +24,14 @@ const circularCountdown = (
   //   Adjusting the circle size
   progressWrapper.style.width = size;
   progressWrapper.style.height = size;
+  // Initial position of progress bar
+  if (startPosition === "up") {
+    progressWrapper.style.transform = "rotate(270deg)";
+  } else if (startPosition === "left") {
+    progressWrapper.style.transform = "rotate(180deg)";
+  } else if (startPosition === "down") {
+    progressWrapper.style.transform = "rotate(90deg)";
+  }
   //  Making our circular countdown work
   let length = progress.getTotalLength();
   progress.style.stroke = color;
@@ -41,8 +50,8 @@ const renderSeconds = (seconds) => {
 
 const init = () => {
   circularCountdown();
-  //   Function parameters: SECONDS, expects a NUMBER so it can make the countdown work, by default it's 60 seconds. ANIMATIONTRANSITION, LINEAR by default, expects a STRING with a transition name such as: linear, ease, ease-in, ease-out, ease-in-out. COLOR, #c39fe0 by default. Expects a STRING with a valid color code (HEX, RGB, HSL). SIZE: 500x500 by default. Expects a single number for both width and height.
-  //   Knowing this, a custom 200x200 10s ease orange circular countdown would look like circularCountdown(10, "ease", "#FC6949", 200)
+  //   Function parameters: SECONDS, expects a NUMBER so it can make the countdown work, by default it's 60 seconds. ANIMATIONTRANSITION, LINEAR by default, expects a STRING with a transition name such as: linear, ease, ease-in, ease-out, ease-in-out. COLOR, #c39fe0 by default. Expects a STRING with a valid color code (HEX, RGB, HSL). SIZE: 500x500 by default. Expects a single number for both width and height. STARTPOSITION: right by default, expects a string with a position (right, left, up, down) for the progress bar to start off
+  //   Knowing this, a custom 500x500 10s ease orange circular countdown would look like: circularCountdown(10, "ease", "#FC6949", 500);
 };
 
 init();
