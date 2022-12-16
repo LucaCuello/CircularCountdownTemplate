bb9b9f1e4ad877d407147898e736e793f097a489
Luca <lucagcuello@gmail.com>
2022-12-16 15:04:57 -0300

    Code optimization

--- js/index.js
+++ js/index.js
@@ -2,56 +2,80 @@ const progressWrapper = document.getElementById("progress-wrapper"),
   progress = document.getElementById("progress"),
   timeSpan = document.getElementById("seconds");
 
-const circularCountdown = (
-  seconds = 60,
-  animationTransition = "linear",
-  color = "#c39fe0",
-  size = 500,
-  startPosition = "right"
-) => {
-  // Rendering the seconds on screen
-  let time = seconds;
-  timeSpan.innerHTML = seconds;
+// If you want to make a custom circular countdown, you need to edit the object named "options".
+// Duration: expects a number && sets the duration of the countdown.
+// Transition: expects a string && sets the transition used in the animation. Examples: "linear, ease, ease-in, ease-out, ease-in-out"
+// Color: expects a string, && sets the color of the circle. Examples: "#c39fe0, rgb(195, 159, 224), hsl(273deg 51% 75%), hwb(273deg 62% 12%)"
+// Size: expects a number && sets the size of the circle.
+// InitialPosition: expects a string && sets the countdown initial position. Examples: "right, left, up, down"
+
+const options = {
+  duration: 60,
+  transition: "linear",
+  color: "#c39fe0",
+  size: 500,
+  initialPosition: "right",
+};
+
+const circularCountdown = ({
+  duration,
+  transition,
+  color,
+  size,
+  initialPosition,
+}) => {
+  // Rendering countdown on HTML
+  renderSeconds(duration);
+  // Adjusting timer font-size depending of countdown size
+  adjustFontSize(size);
+  // Adjusting circular countdown size
+  adjustCircleSize(size);
+  // Setting initial position of countdown
+  setInitialPosition(initialPosition);
+  // Starting animation (setting transition, color and duration)
+  animationStart(color, transition, duration);
+};
+
+const renderSeconds = (duration) => {
+  timeSpan.innerHTML = duration;
   const secondsCountdown = setInterval(() => {
-    time--;
-    renderSeconds(time);
-    if (time === 0) {
+    duration--;
+    timeSpan.innerHTML = duration;
+    if (duration === 0) {
       clearInterval(secondsCountdown);
+      timeSpan.innerHTML = `<i class="fa-solid fa-check"></i>`;
     }
   }, 1000);
-  //   Adjusting timer font-size depending of countdown size
+};
+
+const adjustFontSize = (size) => {
   timeSpan.style.fontSize = `${size / 5}px`;
-  //   Adjusting the circle size
+};
+
+const adjustCircleSize = (size) => {
   progressWrapper.style.width = size;
   progressWrapper.style.height = size;
-  // Initial position of progress bar
-  if (startPosition === "up") {
+};
+
+const setInitialPosition = (initialPosition) => {
+  if (initialPosition === "up") {
     progressWrapper.style.transform = "rotate(270deg)";
-  } else if (startPosition === "left") {
+  } else if (initialPosition === "left") {
     progressWrapper.style.transform = "rotate(180deg)";
-  } else if (startPosition === "down") {
+  } else if (initialPosition === "down") {
     progressWrapper.style.transform = "rotate(90deg)";
   }
-  //  Making our circular countdown work
+};
+
+const animationStart = (color, transition, duration) => {
   let length = progress.getTotalLength();
   progress.style.stroke = color;
   progressWrapper.style.strokeDasharray = length;
-  progressWrapper.style.animation = `progress ${animationTransition} ${seconds}s forwards`;
-};
-
-const renderSeconds = (seconds) => {
-  timeSpan.innerHTML = seconds;
-  if (seconds === 0) {
-    timeSpan.innerHTML = `
-    <i class="fa-solid fa-check"></i>
-    `;
-  }
+  progressWrapper.style.animation = `progress ${transition} ${duration}s forwards`;
 };
 
 const init = () => {
-  circularCountdown();
-  //   Function parameters: SECONDS, expects a NUMBER so it can make the countdown work, by default it's 60 seconds. ANIMATIONTRANSITION, LINEAR by default, expects a STRING with a transition name such as: linear, ease, ease-in, ease-out, ease-in-out. COLOR, #c39fe0 by default. Expects a STRING with a valid color code (HEX, RGB, HSL). SIZE: 500x500 by default. Expects a single number for both width and height. STARTPOSITION: right by default, expects a string with a position (right, left, up, down) for the progress bar to start off
-  //   Knowing this, a custom 500x500 10s ease orange circular countdown would look like: circularCountdown(10, "ease", "#FC6949", 500);
+  circularCountdown(options);
 };
 
 init();
